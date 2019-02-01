3dedcb91a38546ab5dca1482f38afa794fc20e09
nagomuohta <nagomuohtasecond@gmail.com>
2019-02-01 19:46:16 +0900

    Add products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'test#index'
+  resources :products, only: [:index, :new] do
+  end
 end
