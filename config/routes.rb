b86af6b497eae739284c2a5fdaef4996cff74117
nagomuohta <nagomuohtasecond@gmail.com>
2019-02-01 19:31:46 +0900

    Delete sample

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
-  resources :products, only: [:index] do
-  end
-
+  resources :products, only: [:index]
+  resources :users, only: [:index]
 end
