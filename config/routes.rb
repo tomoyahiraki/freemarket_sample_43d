7bb45bfb9f2ff9fccf57ed5e45bb6b54272b50ea
tomoyahiraki <tomoyahiraki0616@gamil.com>
2019-02-02 19:19:33 +0900

    Add login-view
    # What
    メルカリのログインページのビュー実装
    
    # Why
    ログインするために必要なビューのため

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products, only: [:index, :new]
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+  	collection do
+      get 'login'
+    end
+  end
 end
