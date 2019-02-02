0d470a5666503013128ee3e62a720adf95178e09
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-02-02 18:30:37 +0900

    Add side-bar file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resources :products, only: [:index, :new]
-  resources :users, only: [:index]
+  resources :users, only: [:update, :index]
 end
