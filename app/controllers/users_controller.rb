ee75ae8dafe87d85ea5eef6a74162e2bb83c4d62
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-02-04 19:11:20 +0900

    Add credit-card-registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+  end
+
+  def purchase
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products, only: [:new, :show]
-  resources :users, only: [:index, :edit, :update]
+  resources :users, only: [:index, :edit, :update, :show] do
+    collection do
+      get 'purchase'
+    end
+  end
 end
