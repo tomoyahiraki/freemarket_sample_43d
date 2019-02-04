18381ad9a98e38347f928631a54bde9f8c1ec517
Daichi1129 <hamadadaichi@hamadadaichinoMacBook-Air.local>
2019-02-04 13:05:07 +0900

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
-  resources :products, only: [:index, :new, :show, :create]
-  resources :users, only: [:index, :edit]
-=======
+
   root 'products#index'
   resources :products, only: [:new, :show]
   resources :users, only: [:index, :edit, :update] do
@@ -14,5 +9,4 @@ Rails.application.routes.draw do
   end
  end
 
->>>>>>> nagomuohta/master
 end
