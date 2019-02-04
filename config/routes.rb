8df2ef092945a1f6a25041e9a58c43f3de2e93cc
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-02-04 19:34:37 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,19 @@
 Rails.application.routes.draw do
 
   root 'products#index'
-<<<<<<< HEAD
-  resources :products, only: [:new, :show]
-  resources :users, only: [:index, :edit, :update, :show] do
-    collection do
-      get 'purchase'
-      get 'login'
-      get 'logout'
-    end
-  end
-=======
   resources :products, only: [:new, :show] do
     collection do
       get 'buy_confirm'
     end
   end
 
-  resources :users, only: [:index, :edit, :update] do
+  resources :users, only: [:index, :edit, :update, :show] do
     collection do
+      get 'purchase'
       get 'login'
       get 'logout'
       get 'password_less'
     end
   end
 
->>>>>>> nagomuohta/master
 end
