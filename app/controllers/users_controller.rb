112b75d423ee0b8537b42d4a7c239ae725f03365
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-02-04 19:15:22 +0900

    Merge remote-tracking branch 'nagomuohta/master' into Credit-card-registration
    # Conflicts:
    #	app/assets/stylesheets/application.scss
    #	app/controllers/users_controller.rb
    #	config/routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,12 @@ class UsersController < ApplicationController
   def show
   end
 
+  def login
+  end
+
+  def logout
+  end
+
   def purchase
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update, :show] do
     collection do
       get 'purchase'
+      get 'login'
+      get 'logout'
     end
   end
 end
