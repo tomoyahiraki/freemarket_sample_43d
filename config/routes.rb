fb3f6f62bb9e9e2fdb988aca5427f8ac489218ce
Daichi1129 <hamadadaichi@hamadadaichinoMacBook-Air.local>
2019-02-04 12:50:05 +0900

    make products create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  resources :products, only: [:index, :new, :show]
+  root 'products#index'
+  resources :products, only: [:index, :new, :show, :create]
   resources :users, only: [:index, :edit]
 end
