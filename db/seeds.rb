a3c63f14a9e46b8ea5ee27963578269754644da7
Daichi1129 <hamadadaichi@hamadadaichinoMacBook-Air.local>
2019-02-11 15:59:53 +0900

    favorite view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create(title: "商品1", product_old_id: "1",deliveryfee_id: "1",area_id: "1",shipment_id: "1",price: "100",product_introduce: "猫ちゃん",product_state_id: "1",user_id: "1",brand_id: "1",size_id: "1",category_id: "1",deliveryday_id: "1",)
+
+Area.create(name: "北海道")
+
+BirthDay.create(name: "29")
+
+BirthMonth.create(name: "11")
+
+BirthYear.create(name: "1993")
+
+Brand.create(name: "猫ちゃん")
+
+Buy.create(product_id: "1", user_id: "1")
+
+Category.create(name: "動物")
+
+Deliveryday.create(name: "2~3日で配送")
+
+Deliveryfee.create(name: "出品者が負担")
+
+Favorite.create(product_id: "1", user_id: "1")
+
+Image.create(image_url: "https://grapee.jp/wp-content/uploads/32187_main2.jpg", product_id: "1")
+
+ProductOld.create(name: "新品")
+
+ProductState.create(name: "出品中")
+
+Shipment.create(name: "郵送")
+
+Size.create(name: "S")
+
+User.create(name: "1")
+
+UserAddress.create(postal_code: "0000000", area_id: "1",city_name: "横浜市", city_number: "111",building: "111",)
+
