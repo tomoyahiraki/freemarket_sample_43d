7ef93e9f7d70937ca844328df67b250c7ea70721
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-02-18 16:19:35 +0900

    Add bread-crumbs

--- app/models/user_address.rb
+++
@@ -1,4 +0,0 @@
-class UserAddress < ApplicationRecord
-  has_one :user, dependent: :destroy
-  belongs_to :area
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190216110321) do
+ActiveRecord::Schema.define(version: 20190215124530) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -124,39 +124,27 @@ ActiveRecord::Schema.define(version: 20190216110321) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "postal_code", null: false
-    t.integer  "area_id",     null: false
-    t.string   "city_name",   null: false
-    t.string   "city_number", null: false
-    t.string   "building",    null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "user_id"
-  end
-
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                                default: "", null: false
-    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "nickname",                                          null: false
-    t.string   "first_name",                                        null: false
-    t.string   "last_name",                                         null: false
-    t.string   "kana_first_name",                                   null: false
-    t.string   "kana_last_name",                                    null: false
-    t.string   "telphone_number",                      default: "", null: false
+    t.string   "nickname",                            null: false
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "kana_first_name",                     null: false
+    t.string   "kana_last_name",                      null: false
+    t.string   "telphone_number",        default: "", null: false
     t.integer  "birth_year_id"
     t.integer  "birth_month_id"
     t.integer  "birth_day_id"
+    t.integer  "creditcard_id"
     t.integer  "proceed"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
-    t.text     "token_id",               limit: 65535
-    t.string   "customer_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
