91f063502ef3dd95d1ca9645db836a405e1aa9f7
tomoyahiraki <tomoyahiraki0616@gamil.com>
2019-02-20 15:23:41 +0900

    あああ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190220060256) do
+ActiveRecord::Schema.define(version: 20190219124307) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
